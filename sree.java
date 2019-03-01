14d66d7b61b23b0eaafbb80942545b6343ca7483
ramakrishnapadugupati <48020634+ramakrishnapadugupati@users.noreply.github.com>
2019-03-01 19:20:27 +0530

    Create sree.java

---
+++ sree.java
@@ -0,0 +1,13 @@
+<web-app>  
+  
+<servlet>  
+<servlet-name>DownloadServlet</servlet-name>  
+<servlet-class>DownloadServlet</servlet-class>  
+</servlet>  
+  
+<servlet-mapping>  
+<servlet-name>DownloadServlet</servlet-name>  
+<url-pattern>/servlet/DownloadServlet</url-pattern>  
+</servlet-mapping>  
+  
+</web-app>  
